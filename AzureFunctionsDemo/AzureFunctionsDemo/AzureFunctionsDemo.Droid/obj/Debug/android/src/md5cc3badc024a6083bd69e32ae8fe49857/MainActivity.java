22057c7af1d4aa97dd6bb2b5a6d153f4d73073f8
Suthahar <jssuthahar@gmail.com>
2017-12-25 07:26:46 +0530

    Bot Vision Service

---
+++ AzureFunctionsDemo/AzureFunctionsDemo/AzureFunctionsDemo.Droid/obj/Debug/android/src/md5cc3badc024a6083bd69e32ae8fe49857/MainActivity.java
@@ -0,0 +1,47 @@
+package md5cc3badc024a6083bd69e32ae8fe49857;
+
+
+public class MainActivity
+	extends md5b60ffeb829f638581ab2bb9b1a7f4f3f.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("AzureFunctionsDemo.Droid.MainActivity, AzureFunctionsDemo.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("AzureFunctionsDemo.Droid.MainActivity, AzureFunctionsDemo.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
